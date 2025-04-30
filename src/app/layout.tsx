ddc75ebce8a60796dddb47f2ee650b574a8fae3c
Aariya Gage <aariyagage@gmail.com>
2025-04-30 14:46:42 -0700

    added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/react';
+
 
 
 const geistSans = Geist({
@@ -23,7 +25,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased tracking-tight`}>
           {children}
-  
+        <Analytics />
       </body>
     </html>
   );
